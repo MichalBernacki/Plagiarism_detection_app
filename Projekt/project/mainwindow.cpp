f4a7435267c0f7214f48f1837f371f0a17ddb1e2
rriczku <kuba8rychlicki@gmail.com>
2021-01-18 17:34:11 +0100

    Checkboxes added

--- Projekt/project/mainwindow.cpp
+++ Projekt/project/mainwindow.cpp
@@ -34,6 +34,9 @@ MainWindow::MainWindow(QWidget *parent) :
     stateStartup->assignProperty(ui->leLabel, "text", "Load directory to start...");
     stateStartup->assignProperty(ui->cbBox1, "enabled", false);
     stateStartup->assignProperty(ui->cbBox2, "enabled", false);
+    stateStartup->assignProperty(ui->cbBox3, "enabled", false);
+    stateStartup->assignProperty(ui->cbBox4, "enabled", false);
+    stateStartup->assignProperty(ui->cbBox5, "enabled", false);
     stateStartup->assignProperty(ui->frResult, "enabled", false);
 
     stateStartup->addTransition(ui->pbOpen, SIGNAL(clicked()), stateOpen);
@@ -52,6 +55,9 @@ MainWindow::MainWindow(QWidget *parent) :
     stateError->assignProperty(ui->pbStart, "enabled", false);
     stateError->assignProperty(ui->cbBox1, "enabled", false);
     stateError->assignProperty(ui->cbBox2, "enabled", false);
+    stateError->assignProperty(ui->cbBox3, "enabled", false);
+    stateError->assignProperty(ui->cbBox4, "enabled", false);
+    stateError->assignProperty(ui->cbBox5, "enabled", false);
     stateError->assignProperty(ui->taCompare, "enabled", false);
 
     connect(stateError, SIGNAL(entered()), this, SLOT(errorFunction()));
@@ -65,6 +71,9 @@ MainWindow::MainWindow(QWidget *parent) :
     stateView->assignProperty(ui->taCompare, "enabled", false);
     stateView->assignProperty(ui->cbBox1, "enabled", true);
     stateView->assignProperty(ui->cbBox2, "enabled", true);
+    stateView->assignProperty(ui->cbBox3, "enabled", true);
+    stateView->assignProperty(ui->cbBox4, "enabled", true);
+    stateView->assignProperty(ui->cbBox5, "enabled", true);
 
     stateView->addTransition(ui->pbOpen, SIGNAL(clicked()), stateOpen);
     stateView->addTransition(ui->pbStart, SIGNAL(clicked()), stateChoose);
