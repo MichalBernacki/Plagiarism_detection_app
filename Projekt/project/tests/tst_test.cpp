f7d340f8f1a733244fd51d25461d9e0c1cb031a2
Jakub Rychlicki <kuba8rychlicki@gmail.com>
2021-01-19 20:50:43 +0100

    Prepare class tests

--- Projekt/project/tests/tst_test.cpp
+++ Projekt/project/tests/tst_test.cpp
@@ -1,34 +1,50 @@
 #include <QtTest>
 
 // add necessary includes here
+#include "prepare.h"
 
 class Test : public QObject
 {
     Q_OBJECT
 
-public:
-    Test();
-    ~Test();
-
 private slots:
-    void test_case1();
 
+    void test_removeEmptyLines()
+    {
+        Prepare p{"../../project/app/cppkeywords.txt"};
+        std::string sin="\n\n\nDzien dobry\nHello World!\n\n\n\n\nLorem ipsum\n\nTest\n\n\n";
+        std::string sout="Dzien dobry\nHello World!\nLorem ipsum\nTest\n";
+
+        p.removeEmptyLines(sin);
+        QVERIFY(sin==sout);
+    }
+    void test_removeKeywords()
+    {
+        Prepare p{"../../project/app/cppkeywords.txt"};
+        std::string sin="avcreturnintforxDwhiletruefalsetrycatchcaseswitch123 break;";
+        std::string sout="avcxD123 ;";
+
+        p.removeKeywords(sin);
+        QVERIFY(sin==sout);
+    }
+     void test_DeleteComments()
+     {
+        Prepare p{"../../project/app/cppkeywords.txt"};
+        std::string sin="//komentarz w pliku\nLorem ipsum\nDzien dobry, macie ta Colke jeszcze?\nStanooooskiiiiiii\n/*tu jest komentarz\ngagas\nabababa*/\nDalej tekst";
+        std::string sout="\nLorem ipsum\nDzien dobry, macie ta Colke jeszcze?\nStanooooskiiiiiii\n\nDalej tekst";
+
+        p.DeleteComments(sin);
+        QVERIFY(sin==sout);
+     }
+
+     void test_ConstructorTest()
+     {
+         Prepare p{"../../project/app/cppkeywords.txt"};
+         QVERIFY(p.keywords.empty()==false);
+         QVERIFY(p.keywords.size()==63);
+     }
 };
 
-Test::Test()
-{
-
-}
-
-Test::~Test()
-{
-
-}
-
-void Test::test_case1()
-{
-
-}
 
 QTEST_APPLESS_MAIN(Test)
 
