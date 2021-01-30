e7cc780e779f9de673779ed899685f3d2fd79ae5
Jakub Rychlicki <kuba8rychlicki@gmail.com>
2021-01-30 23:08:36 +0100

    File paths above file contents

--- Projekt/project/app/nxndialog.cpp
+++ Projekt/project/app/nxndialog.cpp
@@ -20,6 +20,8 @@ NxNDialog::NxNDialog( QWidget *parent, int x, int y, int opt, Result result):
     qDebug() << QString::fromStdString(result.algorithmName);
     ui->teFile1->setText( (result.bestMatch1->m_OriginalContent).c_str() );
     ui->teFile2->setText(result.bestMatch2->m_OriginalContent.c_str());
+    ui->nFile1->setText((result.bestMatch1->m_Path).c_str());
+    ui->nFile2->setText((result.bestMatch2->m_Path).c_str());
    //qDebug() << QString::fromStdString(result.bestMatch1->m_OriginalContent);
 }
 
